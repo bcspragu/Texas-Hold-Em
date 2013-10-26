c6ead6a6b593918f4aade6c175cd4566186c2cc6
Brandon Sprague <brandon00sprague@gmail.com>
2013-10-26 00:41:38 -0400

    Hand ranking done

--- Dealer.cpp
+++ Dealer.cpp
@@ -9,12 +9,12 @@
 using std::cout;
 using std::endl;
 
-void testCardChecking();
+void testHands();
 void testHand(std::vector<Card> hand);
 
 int main(){
   Dealer d;
-  testCardChecking();
+  testHands();
 }
 
 Dealer::Dealer(){
@@ -63,6 +63,32 @@ bool Dealer::fourOfAKind(std::vector<Card> hand){
     }
   }
   return false;
+}
+
+Value Dealer::valueOfFourOfAKind(std::vector<Card> hand){
+  //Map from Card value to the number of those cards you have
+  std::map<Value, int> cardCounts;
+
+  //Iterate over cards and initialize the map to zero at those values
+  std::vector<Card>::iterator itr;
+  for(itr = hand.begin(); itr != hand.end(); ++itr){
+    cardCounts[(*itr).value] = 0;
+  }
+
+  //Iterate over the cards again and increment the count at that value
+  for(itr = hand.begin(); itr != hand.end(); ++itr){
+    cardCounts[(*itr).value]++;
+  }
+
+  //Iterate through the map and look for a count of 4
+  std::map<Value,int>::iterator mitr;
+  for(mitr = cardCounts.begin(); mitr!=cardCounts.end(); ++mitr){
+    if((*mitr).second == 4){
+      return (*mitr).first;
+    }
+  }
+  //Should never reach this, method only called once 4 of a kind exists
+  return TWO;
 }
 
 bool Dealer::fullHouse(std::vector<Card> hand){
@@ -124,6 +150,32 @@ bool Dealer::threeOfAKind(std::vector<Card> hand){
   return false;
 }
 
+Value Dealer::valueOfThreeOfAKind(std::vector<Card> hand){
+  //Map from Card value to the number of those cards you have
+  std::map<Value, int> cardCounts;
+
+  //Iterate over cards and initialize the map to zero at those values
+  std::vector<Card>::iterator itr;
+  for(itr = hand.begin(); itr != hand.end(); ++itr){
+    cardCounts[(*itr).value] = 0;
+  }
+
+  //Iterate over the cards again and increment the count at that value
+  for(itr = hand.begin(); itr != hand.end(); ++itr){
+    cardCounts[(*itr).value]++;
+  }
+
+  //Iterate through the map and look for a count of 4
+  std::map<Value,int>::iterator mitr;
+  for(mitr = cardCounts.begin(); mitr!=cardCounts.end(); ++mitr){
+    if((*mitr).second == 3){
+      return (*mitr).first;
+    }
+  }
+  //Should never reach this, method only called once 3 of a kind exists
+  return TWO;
+}
+
 bool Dealer::twoPair(std::vector<Card> hand){
   //Map from Card value to the number of those cards you have
   std::map<Value, int> cardCounts;
@@ -148,6 +200,60 @@ bool Dealer::twoPair(std::vector<Card> hand){
     }
   }
   return numPairs == 2;
+}
+
+Value Dealer::higherPair(std::vector<Card> hand){
+  //Map from Card value to the number of those cards you have
+  std::map<Value, int> cardCounts;
+
+  //Iterate over cards and initialize the map to zero at those values
+  std::vector<Card>::iterator itr;
+  for(itr = hand.begin(); itr != hand.end(); ++itr){
+    cardCounts[(*itr).value] = 0;
+  }
+
+  //Iterate over the cards again and increment the count at that value
+  for(itr = hand.begin(); itr != hand.end(); ++itr){
+    cardCounts[(*itr).value]++;
+  }
+
+  int numPairs = 0;
+  Value highestValue = TWO;
+  //Iterate through the map and look for a count of 2
+  std::map<Value,int>::iterator mitr;
+  for(mitr = cardCounts.begin(); mitr!=cardCounts.end(); ++mitr){
+    if((*mitr).second == 2 && (*mitr).first > highestValue){
+      highestValue = (*mitr).first;
+    }
+  }
+  return highestValue;
+}
+
+Value Dealer::lowerPair(std::vector<Card> hand){
+  //Map from Card value to the number of those cards you have
+  std::map<Value, int> cardCounts;
+
+  //Iterate over cards and initialize the map to zero at those values
+  std::vector<Card>::iterator itr;
+  for(itr = hand.begin(); itr != hand.end(); ++itr){
+    cardCounts[(*itr).value] = 0;
+  }
+
+  //Iterate over the cards again and increment the count at that value
+  for(itr = hand.begin(); itr != hand.end(); ++itr){
+    cardCounts[(*itr).value]++;
+  }
+
+  int numPairs = 0;
+  Value lowestValue = ACE;
+  //Iterate through the map and look for a count of 2
+  std::map<Value,int>::iterator mitr;
+  for(mitr = cardCounts.begin(); mitr!=cardCounts.end(); ++mitr){
+    if((*mitr).second == 2 && (*mitr).first < lowestValue){
+      lowestValue = (*mitr).first;
+    }
+  }
+  return lowestValue;
 }
 
 bool Dealer::pair(std::vector<Card> hand){
@@ -175,13 +281,96 @@ bool Dealer::pair(std::vector<Card> hand){
   }
   return numPairs == 1;
 }
+Value Dealer::valueOfPair(std::vector<Card> hand){
+  //Map from Card value to the number of those cards you have
+  std::map<Value, int> cardCounts;
+
+  //Iterate over cards and initialize the map to zero at those values
+  std::vector<Card>::iterator itr;
+  for(itr = hand.begin(); itr != hand.end(); ++itr){
+    cardCounts[(*itr).value] = 0;
+  }
+
+  //Iterate over the cards again and increment the count at that value
+  for(itr = hand.begin(); itr != hand.end(); ++itr){
+    cardCounts[(*itr).value]++;
+  }
+
+  //Iterate through the map and look for a count of 2
+  std::map<Value,int>::iterator mitr;
+  for(mitr = cardCounts.begin(); mitr!=cardCounts.end(); ++mitr){
+    if((*mitr).second == 2){
+      return (*mitr).first;
+    }
+  }
+  //Should never reach this, method only called once 3 of a kind exists
+  return TWO;
+}
 
 Value Dealer::highestValue(std::vector<Card> hand){
   std::sort(hand.begin(),hand.end());
   return hand.back().value;
 }
 
-void testCardChecking(){
+int Dealer::scoreHand(std::vector<Card> hand){
+  int primary = 0;
+  int secondary = 0;
+  int tertiary = 0;
+  if(Dealer::royalFlush(hand)){
+    primary = 9;
+    secondary = 0;
+    tertiary = 0;
+  }
+  else if(Dealer::straightFlush(hand)){
+    primary = 8;
+    secondary = highestValue(hand);
+    tertiary = 0;
+  }
+  else if(Dealer::fourOfAKind(hand)){
+    primary = 7;
+    secondary = valueOfFourOfAKind(hand);
+    tertiary = 0;  //Impossible in texas hold'em
+  }
+  else if(Dealer::fullHouse(hand)){
+    primary = 6;
+    secondary = valueOfThreeOfAKind(hand);
+    tertiary = valueOfPair(hand);
+  }
+  else if(Dealer::flush(hand)){
+    primary = 5;
+    secondary = highestValue(hand);
+    tertiary = 0; //Tie
+  }
+  else if(Dealer::straight(hand)){
+    primary = 4;
+    secondary = highestValue(hand);
+    tertiary = 0; //Tie
+  }
+  else if(Dealer::threeOfAKind(hand)){
+    primary = 3;
+    secondary = valueOfThreeOfAKind(hand);
+    tertiary = highestValue(hand);
+  }
+  else if(Dealer::twoPair(hand)){
+    primary = 2;
+    secondary = higherPair(hand);
+    tertiary = lowerPair(hand);
+  }
+  else if(Dealer::pair(hand)){
+    primary = 1;
+    secondary = valueOfPair(hand);
+    tertiary = highestValue(hand);
+  }
+  else{//High card
+    primary = 0;
+    secondary = highestValue(hand);
+    tertiary = 0;
+  }
+  //Unique hand score, weighted by characteristic importance
+  return primary*10000+secondary*100+tertiary;
+}
+
+void testHands(){
   std::vector<Card> royal;
   royal.push_back(Card("AH"));
   royal.push_back(Card("KH"));
@@ -272,39 +461,49 @@ void testHand(std::vector<Card> hand){
   cout << handString << endl << "Hand is ";
   if(Dealer::royalFlush(hand)){
     cout << "a Royal Flush" << endl;
+    cout << "Score: " << Dealer::scoreHand(hand) << endl;
     return;
   }
   if(Dealer::straightFlush(hand)){
     cout << "a Straight Flush" << endl;
+    cout << "Score: " << Dealer::scoreHand(hand) << endl;
     return;
   }
   if(Dealer::fourOfAKind(hand)){
     cout << "Four of a Kind" << endl;
+    cout << "Score: " << Dealer::scoreHand(hand) << endl;
     return;
   }
   if(Dealer::fullHouse(hand)){
     cout << "a Full House" << endl;
+    cout << "Score: " << Dealer::scoreHand(hand) << endl;
     return;
   }
   if(Dealer::flush(hand)){
     cout << "a Flush" << endl;
+    cout << "Score: " << Dealer::scoreHand(hand) << endl;
     return;
   }
   if(Dealer::straight(hand)){
     cout << "a Straight" << endl;
+    cout << "Score: " << Dealer::scoreHand(hand) << endl;
     return;
   }
   if(Dealer::threeOfAKind(hand)){
     cout << "Three of a Kind" << endl;
+    cout << "Score: " << Dealer::scoreHand(hand) << endl;
     return;
   }
   if(Dealer::twoPair(hand)){
     cout << "Two Pair" << endl;
+    cout << "Score: " << Dealer::scoreHand(hand) << endl;
     return;
   }
   if(Dealer::pair(hand)){
     cout << "a Pair" << endl;
+    cout << "Score: " << Dealer::scoreHand(hand) << endl;
     return;
   }
   cout << "High card value: " << Dealer::highestValue(hand) << endl;
+  cout << "Score: " << Dealer::scoreHand(hand) << endl;
 }
--- Dealer.h
+++ Dealer.h
@@ -29,13 +29,19 @@ class Dealer {
     static bool royalFlush(std::vector<Card> hand);
     static bool straightFlush(std::vector<Card> hand);
     static bool fourOfAKind(std::vector<Card> hand);
+    static Value valueOfFourOfAKind(std::vector<Card> hand);
     static bool fullHouse(std::vector<Card> hand);
     static bool flush(std::vector<Card> hand);
     static bool straight(std::vector<Card> hand);
     static bool threeOfAKind(std::vector<Card> hand);
+    static Value valueOfThreeOfAKind(std::vector<Card> hand);
     static bool twoPair(std::vector<Card> hand);
+    static Value higherPair(std::vector<Card> hand);
+    static Value lowerPair(std::vector<Card> hand);
     static bool pair(std::vector<Card> hand);
+    static Value valueOfPair(std::vector<Card> hand);
     static Value highestValue(std::vector<Card> hand);
+    static int scoreHand(std::vector<Card> hand);
 };
 
 #endif
